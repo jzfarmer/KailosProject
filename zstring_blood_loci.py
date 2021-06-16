72d0afde9d1d5c9a83ad6e98e84a5b02afdcd74d
Jessica Farmer <63081939+jzfarmer@users.noreply.github.com>
2021-06-16 14:27:41 -0700

    Add files via upload

--- zstring_blood_loci.py
+++ zstring_blood_loci.py
@@ -5,15 +5,15 @@ import numpy as np
 # get rid of \t and \n separators
 filename = 'AN_sample1.txt'
 df = pd.read_csv(filename, sep='\t', lineterminator='\r') 
-print(f"{filename} : file read!")
+# print(f"{filename} : file read!")
 df = df.replace('\t',' ', regex=True)
 df = df.replace('\n',' ', regex=True)
-print(df)
+# print(df)
 
 # check the data frame to make sure everything read in correctly
-for (columnName, columnData) in df.iteritems():
-    print('Colunm Name : ', columnName)
-    print('Column Contents : ', columnData.values)
+#for (columnName, columnData) in df.iteritems():
+   # print('Colunm Name : ', columnName)
+   # print('Column Contents : ', columnData.values)
 
 # subset the dataframe by locus 3 and the z string
 locus_3 = df.loc[df['locus'] == 'blood-3-cg08535938-chr1_161128968_161129092', ['consensusZstring_0.9']]
@@ -22,10 +22,17 @@ locus_3 = df.loc[df['locus'] == 'blood-3-cg08535938-chr1_161128968_161129092', [
 z_stringList3 = locus_3.values.tolist()
 # print(z_stringList3)
 
-# is the first z methylated in locus 3? 
+# how many of the first zs are methylated in locus 3? 
 count = 0
-for i in z_stringList3:
-   
+# print(len(z_stringList3))
+for i in range(len(z_stringList3)):
+    print('[{}]{}'.format(i, z_stringList3[i]))
+    print('[{}]{}'.format(i, z_stringList3[i][0]))
+    if z_stringList3[i][0][0] == 'Z':
+        count += 1
+    elif z_stringList3[i][0][0] == 'z':
+       count == 1
+print(count)
 
 
 
